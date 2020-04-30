091fb0478f3ca00f17dd87f5b2726ec444ac74b4
yusufbayup <49264824+yusufbayup@users.noreply.github.com>
2020-05-01 05:57:49 +0700

    https://github.com/yusufbayup/praktikmmobile5

---
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -0,0 +1,68 @@
+package com.example.myapplication;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
+
+    GoogleMap map;
+    ProgressBar pBsr;
+    double mLatitude = 0;
+    double mLongitude = 0;
+    private GoogleMap googleMap;
+    final LatLng spbuumm = new LatLng(-7.9208126,112.594724);
+    private Marker markerBuatansaya;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+
+        // Add a marker in Sydney and move the camera
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                markerBuatansaya= googleMap.addMarker(new MarkerOptions().position((latLng)).title("Haloo").snippet("mbarang bos"));
+
+            }
+        });
+        googleMap.setOnMarkerClickListener(this);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(spbuumm,20));
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Toast.makeText(this, marker.getTitle() + " Sudah diklik ",Toast.LENGTH_SHORT).show();
+        return false;
+    }
+}
